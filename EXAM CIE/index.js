bda2d4c42058b0f6f5ca6d930dde4853b9e3049e
Meet Bhatti <meetbhatti411@gmail.com>
2026-08-26 16:10:41 +0530

    Add files via upload

---
+++ EXAM CIE/index.js
@@ -0,0 +1,10 @@
+let header_class = document.querySelector("#header_class");
+if(header_class)
+{
+   header_class.addEventListener("onmousehover",()=>
+{
+       // document.querySelector(header_class).style.
+});
+   
+
+}
